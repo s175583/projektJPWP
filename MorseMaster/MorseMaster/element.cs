c35f8a75148234d1ce33b6220b45659d3ed86fa4
s175583 <s175583@student.pg.gda.pl>
2020-10-29 21:33:45 +0100

    update

---
+++ MorseMaster/MorseMaster/element.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MorseMaster
+{
+    class element
+    {
+        public string litera;
+        public string[] znak;
+        public int dl;
+        public element(string litera, int dl, string[] znak)
+        {
+            this.litera = litera;
+            this.dl = dl;
+            this.znak = znak;
+        }
+    }
+}
